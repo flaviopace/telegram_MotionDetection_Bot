e68bea52acd4ab318e547c0dc39c8ed02b1f2137
Flavio Pace <flavio.pace@gmail.com>
2022-01-29 19:38:04 +0100

    little improvemen, now python accepts image as input

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,18 +1,25 @@
 import telegram
-import time
 import json
 import os
 import sys
+import argparse
 
 JSON_FILE = 'config.json'
-
 PHOTO_PATH = 'img/sample.png'
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Process Input arguments.')
+    parser.add_argument('--file', dest='fileFullPath',  default=None,
+                        help='Full Image Path')
+    parser.add_argument('--config', dest='configFile', default=JSON_FILE,
+                        help='Full Config JASON Path')
+
+
+    args = parser.parse_args()
     # load Config
-    with open(os.path.join(sys.path[0], JSON_FILE), 'r') as in_file:
+    with open(os.path.join(sys.path[0], args.configFile), 'r') as in_file:
         conf = json.load(in_file)
     # Initialize Bot
     bot = telegram.Bot(token=conf['telegram_bot_config']['token_id'])
     bot.send_message(chat_id=conf['telegram_bot_config']['channel_id'], text="Movimento Rilevato")
-    bot.send_photo(chat_id=conf['telegram_bot_config']['channel_id'], photo=open(PHOTO_PATH, 'rb'))
+    bot.send_photo(chat_id=conf['telegram_bot_config']['channel_id'], photo=open(args.fileFullPath, 'rb'))
